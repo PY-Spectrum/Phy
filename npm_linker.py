25d86276266a7b675648bf8b6eed86b012ccb884
Spectrum <76483535+PY-Spectrum@users.noreply.github.com>
2021-12-30 22:58:17 +0530

    Delete npm_linker.py

--- npm_linker.py
+++
@@ -1,9 +0,0 @@
-"""quadratic eqn calculator"""
-
-
-def eqn(x_2,x,c):
-   soln_1 = (-x+((x**2)-(4*x_2*c))**0.5)/(2*x_2)
-   soln_2= (-x-((x**2)-(4*x_2*c))**0.5)/(2*x_2)
-   soln_tuple = (soln_1,soln_2)
-
-   return soln_tuple
